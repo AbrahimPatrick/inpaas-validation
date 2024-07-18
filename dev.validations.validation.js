431152dd787afa64d476d5ada42aaa8a0b08c8f9
Abrahim Patrick <patrick.almeida.abrahim@gmail.com>
2024-07-18 10:24:47 -0300

    Resolve some warns.

--- dev.validations.validation.js
+++ dev.validations.validation.js
@@ -18,16 +18,18 @@
 
       var container = data;
 
+      var rename = null;
+
       for (var position in keys) {
         if (container[keys[position]] || container[keys[position]] === 0 || container[keys[position]] === false) {
           container = container[keys[position]];
           if (keys.length <= position + 1) {
-            var rename = getRenameValue(container);
+            rename = getRenameValue(container);
             result = execute({ data: container, rules: rules[key], result: result, fieldname: rename || keys[position] });
           }
         } else {
           if (keys.length <= position + 1 && rules[key].indexOf("required") > -1) {
-            var rename = getRenameValue(rules[key]);
+            rename = getRenameValue(rules[key]);
             result = {
               isDataValid: false,
               message: result.message + L10n.translate("label.cst.validation.required").replace(":campo", rename || keys[position]) + ' '
@@ -38,23 +40,23 @@
     }
     return result;
   }
-  
+
   function getRenameValue(str) {
-      if(!str) return;
-      var renameIndex = str.indexOf("rename:");
+    if (!str) return;
+    var renameIndex = str.indexOf("rename:");
 
-      if (renameIndex === -1) {
-          return null; // Não existe 'rename' na string
-      }
+    if (renameIndex === -1) {
+      return null; // Não existe 'rename' na string
+    }
 
-      var start = renameIndex + "rename:".length;
-      var end = str.indexOf("|", start);
+    var start = renameIndex + "rename:".length;
+    var end = str.indexOf("|", start);
 
-      if (end === -1) {
-          end = str.length;
-      }
+    if (end === -1) {
+      end = str.length;
+    }
 
-      return str.substring(start, end);
+    return str.substring(start, end);
   }
 
   function execute(parameters) {
@@ -87,7 +89,7 @@
 
   function required(result, data, fieldname) {
     if ((!data || data === '') && data !== 0 && data !== false) {
-      
+
       return {
         isDataValid: false,
         message: result.message + L10n.translate("label.cst.validation.required").replace(":campo", fieldname) + ' '
